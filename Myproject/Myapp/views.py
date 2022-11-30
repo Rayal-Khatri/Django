89b8f621fb92875030c6bf74822de966134471a6
Rayal kc <kcrayal450@gmail.com>
2022-11-30 19:29:03 +0545

    Url Testing

--- Myproject/Myapp/views.py
+++ Myproject/Myapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hey!</h1>')
+    return HttpResponse('<h1>Hey!This is my first Django Website</h1>')
 
 # Create your views here.
