eabdec0585f2f10f9a69604dda89f354c20acdc3
Rayal kc <kcrayal450@gmail.com>
2022-12-01 22:59:22 +0545

    Displaying another page from front page

--- Myproject/Myapp/views.py
+++ Myproject/Myapp/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    contex={
-        'name':'Rayal',
-        'age' :21,
-        'nationality' : "Nepali"
-    }
-    # name = 'Rayal'
-    return render(request,'index.html',contex)
+    # contex={
+    #     'name':'Rayal',
+    #     'age' :21,                is used to send data as object to the HTML file
+    #     'nationality' : "Nepali"
+    # }
+  # # name = 'Rayal'
+    return render(request,'index.html')
 # Create your views here.
+
+def counter(request):
+    return render(request,'count.html')
