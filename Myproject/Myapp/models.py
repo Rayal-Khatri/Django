bf085e9acf707907e699b30b1f022e479145e9b4
Rayal Khatri <kcrayalgw@gmail.com>
2022-12-03 10:17:10 +0545

    Using Models to display info in frount end

--- Myproject/Myapp/models.py
+++ Myproject/Myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class People:
+    id : int
+    name : str
+    comment : str
+
 # Create your models here.
--- Myproject/Myapp/views.py
+++ Myproject/Myapp/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . models import People 
 def index(request):
-    # contex={
-    #     'name':'Rayal',
-    #     'age' :21,                is used to send data as object to the HTML file
-    #     'nationality' : "Nepali"
-    # }
-  # # name = 'Rayal'
-    return render(request,'index.html')
+    person1 = People()
+    person1.name ="Subodh Dahal"
+    person1.comment =" This is a super useful website"
+
+    person2 = People()
+    person2.name ="Barsha Rai"
+    person2.comment =" The UI is super simple to use"
+    
+    person3 = People()
+    person3.name ="Rizen Shakya"
+    person3.comment =" I dont even read book but i use this Website"
+    
+    person4 = People()
+    person4.name ="Sujal Maharjan"
+    person4.comment =" I am super dum and only thing smart i do is use this site"
+    
+    persons =[person1,person2,person3,person4]
+    return render(request,'index.html',{'persons':persons})
 # Create your views here.
 
-def counter(request):
-    text=request.POST['text']          #Post is for private data  
-    no_of_words = len(text.split())
-    context={
-        'words' : no_of_words,
-        'content' : text
-    }
-    return render(request,'count.html',context)
+# def counter(request):
+#     text=request.POST['text']          #Post is for private data  
+#     no_of_words = len(text.split())
+#     context={
+#         'words' : no_of_words,
+#         'content' : text
+#     }
+#     return render(request,'count.html',context)
