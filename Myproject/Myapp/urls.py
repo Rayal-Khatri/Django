827f04668bcc4bd6c33c0b1b555b62a7ca29a996
Rayal Khatri <kcrayalgw@gmail.com>
2022-12-05 21:21:53 +0545

    Register a user in site

--- Myproject/Myapp/urls.py
+++ Myproject/Myapp/urls.py
@@ -4,5 +4,6 @@ from.import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('counter',views.counter,name='counter'),
-    path('register',views.register, name='register')
+    path('register',views.register, name='register'),
+    path('login',views.login,name='login')
 ]
--- Myproject/Myapp/views.py
+++ Myproject/Myapp/views.py
@@ -17,11 +17,32 @@ def counter(request):
 
 def register(request):
     if request.method =="POST":
-        username = request.POST('username')
-        email = request.POST('email')
-        password = request.POST('pw')
-        password2 = request.POST('pw2')
-    return render(request,'register.html' )
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['pw']
+        password2 = request.POST['pw2']
+
+        if password==password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username already in use')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request,'Password Incorrect!')
+            return redirect('register')
+    else:
+        return render(request,'register.html' )
+
+
+def login(request):
+    return render(request,'login.html')
+
 # def counter(request):
 #     text=request.POST['text']          #Post is for private data  
 #     no_of_words = len(text.split())
