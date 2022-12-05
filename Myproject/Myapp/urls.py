d7e34e048ce29ca0756f23d1788897d60a6469f3
Rayal Khatri <kcrayalgw@gmail.com>
2022-12-05 21:01:08 +0545

    Trying to get user to Register

--- Myproject/Myapp/urls.py
+++ Myproject/Myapp/urls.py
@@ -3,5 +3,6 @@ from.import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('counter',views.counter,name='counter')
+    path('counter',views.counter,name='counter'),
+    path('register',views.register, name='register')
 ]
--- Myproject/Myapp/views.py
+++ Myproject/Myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from . models import Features 
 from . models import Namesofpeople 
 
@@ -12,6 +14,14 @@ def counter(request):
     name = request.POST['name']
     message = request.POST['message']
     return render(request,'responce.html',{'name':name,'msg':message})
+
+def register(request):
+    if request.method =="POST":
+        username = request.POST('username')
+        email = request.POST('email')
+        password = request.POST('pw')
+        password2 = request.POST('pw2')
+    return render(request,'register.html' )
 # def counter(request):
 #     text=request.POST['text']          #Post is for private data  
 #     no_of_words = len(text.split())
