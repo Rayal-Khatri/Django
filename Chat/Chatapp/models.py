ebf37998f1b9f0f9f52558ffb78d790b2781c956
Rayal Khatri <kcrayalgw@gmail.com>
2022-12-20 20:56:49 +0545

    Chat App Configure pages Day_1

--- Chat/Chatapp/models.py
+++ Chat/Chatapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+
+class Message(models.Model):
+    message = models.CharField(max_length=10000)
--- Chat/Chatapp/views.py
+++ Chat/Chatapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def chat(request):
+    return render(request,'Room.html')
